df23043b6ebce9c26cd1b04c3d6f5d9641e35308
Nikita Tyrkin <tyrkinn@pm.me>
2023-10-22 01:10:41 +0300

    feat: valid credentials

--- src/shared/api/rtkApi.ts
+++ src/shared/api/rtkApi.ts
@@ -7,14 +7,15 @@ import { USER_LOCAL_STORAGE_KEY } from '@/shared/consts/localStorage'
 export const rtkApi = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://752b-46-242-10-177.ngrok-free.app/api#/',
+        baseUrl: 'https://248c-46-242-10-177.ngrok-free.app/',
         prepareHeaders: (headers) => {
             const token =
                 localStorage.getItem(
                     USER_LOCAL_STORAGE_KEY
                 ) || ''
             if (token) {
-                headers.set('Authorization', token)
+                headers.set('Authorization', token.replace(/"/g, ""))
+                headers.set('ngrok-skip-browser-warning', 'true')
             }
             return headers
         },
